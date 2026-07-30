5e4c4a2ba854056dc2238da972de012f5c604afc
AkashPawar122 <akashrameshpawar666@gmail.com>
2026-07-30 19:17:08 +0530

    Update logo

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "GitPulse",
   description: "GitHub year-in-review card for developers",
+  icons: {
+    icon: "/logo.svg",
+  },
 };
 
 export default function RootLayout({ children }) {
